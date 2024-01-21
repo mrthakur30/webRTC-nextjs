cba1d89989d14d52e13edca07572d06fd2fa6c7f
Mukul Thakur <95705739+mrthakur30@users.noreply.github.com>
2024-01-21 18:08:55 +0530

    Update SocketProvider.tsx
    changed url

--- client/src/context/SocketProvider.tsx
+++ client/src/context/SocketProvider.tsx
@@ -17,7 +17,7 @@ interface SocketProviderProps {
 }
 
 export const SocketProvider: React.FC<SocketProviderProps> = ({ children }) => {
-  const socket = useMemo(() => io('http://localhost:8000'), []);
+  const socket = useMemo(() => io('https://webrtc-backend-ja48.onrender.com'), []);
 
   return (
     <SocketContext.Provider value={socket}>
